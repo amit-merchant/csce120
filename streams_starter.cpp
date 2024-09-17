ba5c096b0af582575ea33abb9a0139737d7838ae
amit-merchant <amit.merchant@tamu.edu>
2024-09-17 08:33:11 -0500

    Add files via upload

---
+++ streams_starter.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+using std::ifstream, std::ofstream, std::cin, std::cout, std::endl, std::string;
+
+// read from an input file "TXCitiesUnder100.txt"
+// and write into an output file backwards "TXCitiesUnder100Copy.txt"
+// line 1 becomes line last, line last becomes line 1
+
+int main(){
+    const int CAPACITY = 20;
+    // TODO ifstream statement
+    // TODO ofstream statement
+
+    string smallTowns[CAPACITY];
+    int i=0;
+    while (true) {
+        // TODO read from input stream handler into smallTowns[i]
+        if (smallTowns[i] == "\0") {break;}
+        i++;
+    }
+    int max_cities = i;
+    cout << max_cities;
+    for (; ; i--) {
+        // TODO write onto output stream from smallTowns[i]
+    }
+}
