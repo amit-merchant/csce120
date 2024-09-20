0e128dcc882696f8971ea28d73ed3e69a7b84081
amit-merchant <amit.merchant@tamu.edu>
2024-09-19 19:04:22 -0500

    Add files via upload

---
+++ exam2/stream_ex_2_starter.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using std::ifstream, std::ofstream, std::cin, std::cout, std::endl, std::string;
+
+
+int main(){
+    const int SIZE = 5;
+    // TODO declare input file stream to read from "VariousNumbers.txt"
+
+    unsigned int Numbers[SIZE];
+    for (int i = 0; i < SIZE; ++i) {
+        // TODO use the stream handler to read into the array 
+    }
+
+    int largest_number = 0;
+    for (int i=0; i < SIZE; ++i) {
+        // TODO logic to determine if the array element is larger than
+        // largest_number, if yes update largest_number
+    }
+    cout << "largest number was... " << largest_number << endl;
+}
