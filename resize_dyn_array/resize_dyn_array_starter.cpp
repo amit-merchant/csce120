c2a914ac496bd6cb9ad134097a0fd8dbdbf9cd05
amit-merchant <amit.merchant@tamu.edu>
2024-10-22 16:01:58 -0500

    Update resize_dyn_array_starter.cpp

--- resize_dyn_array/resize_dyn_array_starter.cpp
+++ resize_dyn_array/resize_dyn_array_starter.cpp
@@ -2,8 +2,7 @@
 #include <fstream>
 using namespace std;
 
-void resize (int **&array, const int rows, const int addRows, 
-    const int cols, const int addCols, const int value) {
+void resize (...) {
     // TODO complete this function
 
     // Resize array per instruction
@@ -12,24 +11,13 @@ void resize (int **&array, const int rows, const int addRows,
 }
 
 // do not touch this function
-void printarray(int **array, const int rows, const int cols) {
-    for (int i=0; i < rows; i++) {
-        for (int j=0; j < cols; j++) {
-            cout << array[i][j] << " "; 
-        }
-        cout << endl;
-    }
+void printarray(...) {
+
 }
 
 int main () {
     // initial array size 2x3
-    const int numRows = 2;
-    const int numCols = 3;
-    int **arry = new int*[numRows]; 
-    for (int i=0; i < numRows; i++) {
-        arry[i]  = new int[numCols] {3+i, 5+i, 7+i};
-    }
-    printarray(arry, numRows, numCols);
+
     // TODO read three numbers from new1.txt
     
     // TODO error message if you cannot open the file or it has less than three numbers
@@ -39,5 +27,4 @@ int main () {
     // TODO invoke resize function
 
     // TODO print new array
-    
 }
