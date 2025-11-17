b9d8066985f7b0327d805860e6d76f2be011e501
amit-merchant <amit.merchant@tamu.edu>
2025-11-17 06:41:07 -0600

    Create linkedlist_rule3.cpp

---
+++ final/linkedlist_rule3.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class node {
+    public:
+        int value;
+        node *next;
+    public:
+        node (int val, node *ptr) : value(val), next(ptr) {}    
+        node (int val) : value(val), next(nullptr) {}    
+    };
+
+class linkedlist {
+    public:
+        node *head;
+    public:
+        linkedlist () : head(nullptr){}
+        void add(node *ref, int val) {
+            node *newnode = new node(val, ref->next);
+            ref->next = newnode;
+        } // adds a new node with val after the node ref
+        void remove(node *ref) {
+            node *tmp = ref->next->next;
+            delete ref->next;
+            ref->next = tmp;    
+        } // removes the node after ref
+        node* access(int val) {
+            node* currnode = head;
+            while (currnode != nullptr && currnode->value != val) {
+                currnode = currnode->next;
+            }
+            return currnode;
+        } // returns the node that first matches the val
+        void print() {
+            node* currnode = head;
+            while (currnode != nullptr) {
+                cout << currnode->value << " ";
+                currnode = currnode->next;
+            }
+            cout << endl;
+        }
+        ~linkedlist() {
+            node* currnode = head;
+            while (currnode != nullptr) {
+                node* tmp = currnode;
+                currnode = currnode->next;
+                delete tmp;
+            }
+            head = nullptr;
+        } // destructor
+};
+
+int main() {
+    cout << "using default copy and assignment costructors which are shallow copies" << endl;
+    linkedlist ll;
+    ll.head = new node(0,nullptr);
+    ll.add(ll.head, 1);
+    ll.add(ll.head->next, 2);
+    ll.add(ll.head->next->next, 3);
+    cout << "created original ll: ";
+    ll.print();
+    linkedlist copyofll(ll);
+    cout << "created copy of ll: ";
+    copyofll.print();
+
+    cout << "making modifications..." << endl;
+    node* currnode = ll.access(2);
+    ll.remove(currnode);
+    cout << "printing original ll again: ";
+    ll.print();
+    cout << "printing copy of ll again: ";
+    copyofll.print();
+    cout << "making modifications..." << endl;
+    currnode = ll.access(1);
+    ll.remove(currnode);
+    ll.print();
+    cout << "assigning original to copy of..." << endl;
+    ll = copyofll;
+    ll.print();
+}
